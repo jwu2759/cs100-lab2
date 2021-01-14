eb31f86e72ba39f738d08416da504d3354694de4
ryanchangx <ryanchangx@gmail.com>
2021-01-13 19:12:58 -0800

    Fixed c-count.h file

--- c-count.h
+++ c-count.h
@@ -2,7 +2,7 @@
 // count function should go here
 
 unsigned int count(const std::string& str){
-	unsigned int count = 0;
+	unsigned int counter = 0;
 	bool on_space = true;
 	
 	for(int i = 0; i < str.size(); i++){
